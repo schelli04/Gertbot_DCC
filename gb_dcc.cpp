b01eab809e385b5ede75e28739e53ce598fe829e
schelli04 <martin@hohmann-online.org>
2015-06-10 14:22:49 +0200

    Fehler korrigiert

--- gb_dcc.cpp
+++ gb_dcc.cpp
@@ -83,14 +83,14 @@ gb_dcc::gb_dcc(QWidget *parent, Qt::WFlags flags)
   connect(ui.pb_Stop_loc2 ,SIGNAL(clicked(bool)), this, SLOT(loc2_stop_slot()));
   connect(ui.pb_Stop_loc3 ,SIGNAL(clicked(bool)), this, SLOT(loc3_stop_slot()));
   
-  connect(ui.spB_loco1 ,SIGNAL(valueChanged(int)), this, SLOT(loc1_spB_slot(char)));
-  connect(ui.spB_loco2 ,SIGNAL(valueChanged(int)), this, SLOT(loc2_spB_slot(char)));
-  connect(ui.spB_loco3 ,SIGNAL(valueChanged(int)), this, SLOT(loc3_spB_slot(char)));
+  connect(ui.spB_loco1 ,SIGNAL(valueChanged(int)), this, SLOT(loc1_spB_slot(int)));
+  connect(ui.spB_loco2 ,SIGNAL(valueChanged(int)), this, SLOT(loc2_spB_slot(int)));
+  connect(ui.spB_loco3 ,SIGNAL(valueChanged(int)), this, SLOT(loc3_spB_slot(int)));
   
-  connect(ui.chkBx_Chn0, SIGNAL(valueChanged()), this, SLOT(chn_chkBx_slot()));
-  connect(ui.chkBx_Chn1, SIGNAL(valueChanged()), this, SLOT(chn_chkBx_slot()));
-  connect(ui.chkBx_Chn2, SIGNAL(valueChanged()), this, SLOT(chn_chkBx_slot()));
-  connect(ui.chkBx_Chn3, SIGNAL(valueChanged()), this, SLOT(chn_chkBx_slot()));
+  connect(ui.chkBx_Chn0, SIGNAL(stateChanged()), this, SLOT(chn_chkBx_slot()));
+  connect(ui.chkBx_Chn1, SIGNAL(stateChanged()), this, SLOT(chn_chkBx_slot()));
+  connect(ui.chkBx_Chn2, SIGNAL(stateChanged()), this, SLOT(chn_chkBx_slot()));
+  connect(ui.chkBx_Chn3, SIGNAL(stateChanged()), this, SLOT(chn_chkBx_slot()));
   
   connect(ui.sld_loc1,SIGNAL(valueChanged(int)), this, SLOT(loc1_slide_slot(int)));
   connect(ui.sld_loc2,SIGNAL(valueChanged(int)), this, SLOT(loc2_slide_slot(int)));
